60ab7b07b98409b90d8c95eadd604f0498790a25
Luka <lukacuturilo2709@gmail.com>
2025-07-13 14:32:16 +0200

    Add files via upload

---
+++ SimEnka.py
@@ -0,0 +1,97 @@
+import sys
+from collections import deque
+
+def parse_input():
+    lines = []
+    for line in sys.stdin:
+        lines.append(line.strip())
+
+    inputs = lines[0].split('|')
+    states = lines[1].split(',')       
+    start_state = lines[4]
+    transitions = {}
+
+    for line in lines[5:]:
+        if not line:
+            continue
+
+        left, right = line.split('->')
+        current_state, symbol = left.split(',')
+
+        if current_state not in transitions:
+            transitions[current_state] = {}
+
+        if symbol not in transitions[current_state]:
+            transitions[current_state][symbol] = set()
+
+        if right == '#':
+            next_states = set()
+        else:
+            next_states = set(right.split(','))
+
+        for state in next_states:
+            transitions[current_state][symbol].add(state)
+
+    return inputs, start_state, transitions
+
+def epsilon_closure(state_set, transitions):
+    closure = set(state_set)
+    queue = deque(state_set)
+
+    while queue:
+        current = queue.popleft()
+
+        if current in transitions and '$' in transitions[current]:
+            for next_state in transitions[current]['$']:
+                if next_state not in closure:
+                    closure.add(next_state)
+                    queue.append(next_state)
+
+    return closure
+
+def run_nfa(inputs, start_state, transitions):
+    results = []
+
+    for input_string in inputs:
+        symbols = input_string.split(',') if input_string else []
+        start_set = {start_state}
+        current_states = epsilon_closure(start_set, transitions)
+
+        if current_states:
+            sorted_states = sorted(current_states)
+            first_output = ','.join(sorted_states)
+        else:
+            first_output = '#'
+
+        result_sequence = [first_output]
+
+        for symbol in symbols:
+            next_states = set()
+
+            for state in current_states:
+                if state in transitions and symbol in transitions[state]:
+                    next_states.update(transitions[state][symbol])
+
+            current_states = epsilon_closure(next_states, transitions)
+
+            if current_states:
+                sorted_states = sorted(current_states)
+                new_output = ','.join(sorted_states)
+            else:
+                new_output = '#'
+
+            result_sequence.append(new_output)
+
+        results.append('|'.join(result_sequence))
+
+    return results
+
+def main():
+    inputs, start_state, transitions = parse_input()
+    results = run_nfa(inputs, start_state, transitions)
+
+    for result_line in results:
+        print(result_line)
+
+if __name__ == "__main__":
+    main()
